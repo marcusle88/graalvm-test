02444bfedd9489e377cbdb41dad7ebedb81bc0aa
glohow-marcus <marcus.le@glohow.com>
2025-06-02 11:14:53 +0700

    first commit

---
+++ src/test/java/com/glohow/graalvmtest/GraalvmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glohow.graalvmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
